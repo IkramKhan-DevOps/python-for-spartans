edb8978d3401944253c10352ec622a3227241259
IkramKhan-DevOps <ikram.khan0762@gmail.com>
2022-02-13 13:27:51 +0500

    PROJECT: refreshing repo

--- lists/main.py
+++
@@ -1,4 +0,0 @@
-from lists import list_functions
-
-if __name__ == '__main__':
-    list_functions.list_simple()
--- repetations/main.py
+++
@@ -1,8 +0,0 @@
-from repetations.simple_conditions import (
-    func_for, func_while, sum_100, is_prime
-)
-
-if __name__ == '__main__':
-    is_prime(27)
-    is_prime(10)
-    is_prime(7)
