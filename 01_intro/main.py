6851e6f842be6d6601e5153f2112ac657da79cbe
IkramKhan-DevOps <ikram.khan0762@gmail.com>
2022-02-14 15:11:40 +0500

    LECTURE1: intro, datatypes, operators

---
+++ 01_intro/main.py
@@ -0,0 +1,22 @@
+"""
+Course : Python
+Lecture: 1
+Topics : data types, outputs and inputs
+
+"""
+
+
+print(2+2)
+print(2*2)
+print(2-2)
+print(2/2)
+print(2**2)
+print(2//2)
+print("_______________________________________________")
+
+name = "Ali"
+
+print(name)
+print(type(name))
+print(id(name))
+
---
+++ 02_variables_and_datatypes/main.py
@@ -0,0 +1,38 @@
+
+# DECLARE AND ASSIGNMENT (=)
+first_name = "danish"
+last_name = "karim"
+country = "USA"
+city = "Florida"
+is_married = False
+batch = "FA20"
+age = 55
+gpa = 0.1
+is_failed = True
+comment = "Badly Failed"
+skills = ["html", "css", "js", "python", "react"]
+
+# MULTIPLE DECLARATION
+roll_no, name, email = 12, "Ikram", "ikram.khan0762@gmail.com"
+
+# PRINT, len(), type(), int()
+print("FName    :", first_name, id(first_name), type(first_name))
+print("LName    :", last_name, id(last_name), type(last_name))
+print("Country  :", country, id(country), type(country))
+print("City     :", city, id(city), type(city))
+print("Martial  :", is_married, id(is_married), type(is_married))
+print("Batch    :", batch, id(batch), type(batch))
+print("Age      :", age, id(age), type(age))
+print("GPA      :", gpa, id(gpa), type(gpa))
+print("Status   :", is_failed, id(is_failed), type(is_failed))
+print("comment  :", comment, id(comment), type(comment))
+print("Skills   :", skills, id(skills), type(skills))
+print("")
+
+print(len("danish is a good boy"))
+print(len(skills))
+
+
+
+
+
