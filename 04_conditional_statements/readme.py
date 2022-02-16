e6748baaa943ad667f6f4a814a9a5dfc79754675
IkramKhan-DevOps <ikram.khan0762@gmail.com>
2022-02-16 15:10:19 +0500

    PROJECT: repetation control

---
+++ 04_conditional_statements/readme.py
@@ -0,0 +1,19 @@
+"""
+PROBLEM
+=======
+0: start
+1: User input -> name, pass
+2: condition  -> name=admin pass=admin
+3: successful -> message "Welcome user"
+4: error      -> error "Wrong username and password"
+5: end
+-----------------------------------------------------
+
+TYPES
+=====
+IF
+IF-ELSE
+IF-ELIF-ELSE
+
+
+"""
---
+++ 05_repetation_control_statements/readme.py
@@ -0,0 +1,19 @@
+"""
+RC-Statements
+=============
+
+For   -> foreach -> (sequences)
+While -> for
+range()
+use
+break
+continue
+
+
+DECLERATION
+===========
+
+for value in sequence:
+while condition:
+
+"""
