92dc557394abfd412628ddb3464f88267a92abfc
IkramKhan-DevOps <ikram.khan0762@gmail.com>
2022-02-19 15:32:37 +0500

    LECTURE: functions and sequences

---
+++ C06_functions/readme.py
@@ -0,0 +1,15 @@
+"""
+
+Functions
+---------
+0. What is Function --
+1. Function Declaration + Pass Keyword --
+2. Function (no args)
+3. Function (args)
+4. Function (no args) -> return
+5. Function (args) -> return
+6. Default args
+7. Arbitrary args (*args)
+8. Keyword args (**kwargs)
+
+"""
